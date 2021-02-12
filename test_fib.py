ff3685f93d926ba0f48957d4da040d57c09fe39b
Jeff Taylor-Chang <jeff@jefftc.com>
2021-02-12 15:59:49 -0500

    Updated fib code

--- test_fib.py
+++ test_fib.py
@@ -1,8 +1,11 @@
 def fib(n):
+    """
+    A recursive implementation of finding the nth number in the fibonacci sequence
+    """
     if n <= 1:
-        return 1
+        return n
 
-    return n * fib(n - 1)
+    return fib(n - 1) + fib(n - 2)
 
 
-print(fib(4))
+print(fib(10))
